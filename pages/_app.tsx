39444700d0d6a57454717f89efca68d4a1ad38ad
Kien Ngo <kienngo@skiff.com>
2024-03-08 20:58:33 +0700

    Update

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { createThirdwebClient } from "thirdweb";
-import { ThirdwebProvider } from "thirdweb/react";
+import { ThirdwebProvider, coinbaseConfig, embeddedWalletConfig, metamaskConfig, rainbowConfig, walletConnectConfig, zerionConfig } from "thirdweb/react";
 
 const client = createThirdwebClient({
   clientId: "cf8eb300bba906c5f7325405d00452a0",
@@ -9,7 +9,14 @@ const client = createThirdwebClient({
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ThirdwebProvider client={client}>
+    <ThirdwebProvider client={client} wallets={[
+      metamaskConfig(),
+      coinbaseConfig(),
+      walletConnectConfig(),
+      rainbowConfig(),
+      zerionConfig(),
+      embeddedWalletConfig()
+    ]}>
       <Component {...pageProps} />
     </ThirdwebProvider>
   );
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,4 @@
-import Image from "next/image";
-import { Inter } from "next/font/google";
 import { ConnectButton } from "thirdweb/react";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
