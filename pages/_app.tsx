a612fe5f1592c5d56c19001184b730b9e5d0880d
Kien Ngo <kienngo@skiff.com>
2024-03-08 20:51:29 +0700

    Update

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,16 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { createThirdwebClient } from "thirdweb";
+import { ThirdwebProvider } from "thirdweb/react";
+
+const client = createThirdwebClient({
+  clientId: "cf8eb300bba906c5f7325405d00452a0",
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <ThirdwebProvider client={client}>
+      <Component {...pageProps} />
+    </ThirdwebProvider>
+  );
 }
